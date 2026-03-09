048a267a7c14c01482ae9ece885386e493687695
Ayushthelearner7 <ayush20804@gmail.com>
2026-03-09 14:59:50 +0530

    again

--- Chatmodels/2_chatmodel.py
+++ Chatmodels/2_chatmodel.py
@@ -1,13 +1,14 @@
+import os
 from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
 from dotenv import load_dotenv
 
 load_dotenv()
 
+
+
 llm = HuggingFaceEndpoint(
     repo_id="meta-llama/Llama-3.1-8B-Instruct", # Example of a supported model
-    task="text-generation",
-    provider="auto", # Tells HF to find ANY available server
-    max_new_tokens=512
+    task="text-generation"
 )
 
 model = ChatHuggingFace(llm=llm)
